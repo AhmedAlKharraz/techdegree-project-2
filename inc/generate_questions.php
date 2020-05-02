8382482784bee70eeedd8ff012e628dea7b16b34
AhmedAlKharraz <64503987+AhmedAlKharraz@users.noreply.github.com>
2020-05-03 00:22:46 +0400

    First upload of Project 2

---
+++ inc/generate_questions.php
@@ -0,0 +1,22 @@
+<?php
+// Report simple running errors
+error_reporting(E_ALL);
+// Make sure they're on screen
+ini_set('display_errors', 1);
+// HTML formatted errors
+ini_set("html_errors", 1);
+
+// Generate random questions
+
+// Loop for required number of questions
+
+// Get random numbers to add
+
+// Calculate correct answer
+
+// Get incorrect answers within 10 numbers either way of correct answer
+// Make sure it is a unique answer
+
+// Add question and answer to questions array
+
+?>
---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+// Report simple running errors
+error_reporting(E_ALL);
+// Make sure they're on screen
+ini_set('display_errors', 1);
+// HTML formatted errors
+ini_set("html_errors", 1);
+?>
+<?php 
+include('inc/quiz.php'); 
+//var_dump($_POST["answer"]) . "<br />";
+//var_dump($_POST["index"]) . "<br />";
+//var_dump($_SESSION) . "<br />";
+var_dump($_SESSION['used_indexes']) . "<br />";
+//var_dump($totalQuestions);
+//var_dump($_SESSION['totalCorrect']);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Math Quiz: Addition</title>
+    <link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <div class="container">
+        <div id="quiz-box">
+            <?php 
+            if (!empty($toast)){
+                echo "<p>" . $toast . "</p>";
+            }
+            if ($show_score == true){
+                echo "<p>You got " 
+                . $_SESSION['totalCorrect'] 
+                . " of " 
+                . $totalQuestions 
+                . " correct!</p>";
+
+            }
+            ?>
+            <p class="breadcrumbs"><?php echo "Question " 
+            . count($_SESSION['used_indexes'])
+            . " of " 
+            . $totalQuestions ?></p>
+
+            <p class="quiz" <?php echo $show_score ? 'style="display:none"' : 'style="display:block"'; ?>>
+                                <?php echo "What is " 
+                                . $question['leftAdder'] 
+                                . " + " 
+                                . $question['rightAdder'] 
+                                . " ?"; ?> </p>
+            <form action="index.php" method="post" <?php echo $show_score ? 'style="display:none"' : 'style="display:block"'; ?>>
+                <input type="hidden" name="index" value= <?php echo $index; ?> />
+                <input type="submit" class="btn" name="answer" value= <?php echo $answers[0]; ?> />
+                <input type="submit" class="btn" name="answer" value= <?php echo $answers[1]; ?> />
+                <input type="submit" class="btn" name="answer" value= <?php echo $answers[2]; ?> />
+            </form>
+        </div>
+    </div>
+</body>
+</html>
